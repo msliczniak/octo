48f4765345d70766b0141c8cda0fd9bf6944af10
mzs <mzs@laptop>
2020-10-18 20:56:46 -0500

    POPREGS still needs some work
    $ ./chip8
    
    X # X
    y # Y
    z # Z
    __CHIP8_REGS_FOO # REGSLVL(`FOO')
    __CHIP8_REGS_BAR # REGSLVL(`BAR')
    
      # REGS FOO,9
      # PUSHREG v9 FOO,X
      # PUSHREG va FOO,Y
    v9 # X
    va # Y
    z # Z
    11 # REGSLVL(`FOO')
    __CHIP8_REGS_BAR # REGSLVL(`BAR')
    
      # REGS BAR,11
      # PUSHREG vb BAR,Z
      # PUSHREG vc BAR,Y
    v9 # X
    vc # Y
    vb # Z
    11 # REGSLVL(`FOO')
    13 # REGSLVL(`BAR')
    
      # POPREGS BAR,12
    
    v9 # X
    va # Y
    vb # Z
    11 # REGSLVL(`FOO')
    12 # REGSLVL(`BAR')
    
      # POPREGS BAR,11
    
    v9 # X
    va # Y
    z # Z
    11 # REGSLVL(`FOO')
    11 # REGSLVL(`BAR')
    
      # POPREGS FOO,9
    
    v9 # X
    y # Y
    z # Z
    10 # REGSLVL(`FOO')
    11 # REGSLVL(`BAR')
    
    v9 # X
    y # Y
    z # Z
    10 # REGSLVL(`FOO')
    11 # REGSLVL(`BAR')

--- onetenth/chip8.m
+++ onetenth/chip8.m
@@ -8,47 +8,52 @@ define(`GT',`dnl
   vF -= $1
   if vF == 0')dnl
 dnl
-define(`_R0',  `v0`'')dnl
-define(`_R1',  `v1`'')dnl
-define(`_R2',  `v2`'')dnl
-define(`_R3',  `v3`'')dnl
-define(`_R4',  `v4`'')dnl
-define(`_R5',  `v5`'')dnl
-define(`_R6',  `v6`'')dnl
-define(`_R7',  `v7`'')dnl
-define(`_R8',  `v8`'')dnl
-define(`_R9',  `v9`'')dnl
-define(`_R10', `va`'')dnl
-define(`_R11', `vb`'')dnl
-define(`_R12', `vc`'')dnl
-define(`_R13', `vd`'')dnl
-define(`_R14', `ve`'')dnl
+define(`__CHIP8_R0',  `v0`'')dnl
+define(`__CHIP8_R1',  `v1`'')dnl
+define(`__CHIP8_R2',  `v2`'')dnl
+define(`__CHIP8_R3',  `v3`'')dnl
+define(`__CHIP8_R4',  `v4`'')dnl
+define(`__CHIP8_R5',  `v5`'')dnl
+define(`__CHIP8_R6',  `v6`'')dnl
+define(`__CHIP8_R7',  `v7`'')dnl
+define(`__CHIP8_R8',  `v8`'')dnl
+define(`__CHIP8_R9',  `v9`'')dnl
+define(`__CHIP8_R10', `va`'')dnl
+define(`__CHIP8_R11', `vb`'')dnl
+define(`__CHIP8_R12', `vc`'')dnl
+define(`__CHIP8_R13', `vd`'')dnl
+define(`__CHIP8_R14', `ve`'')dnl
 dnl
 dnl REGS(regset, oregset)
 dnl
 define(`REGS', `dnl
   # REGS $*
-pushdef(`_'`$1', $2)dnl')dnl
+pushdef(`__CHIP8_REGS_'`$1', $2)dnl')dnl
 dnl
 dnl PUSHREG(regset, name)
 dnl
 define(`PUSHREG', `dnl
-ifelse(_$1, `15', `errprint(`PUSHREG: too many registers: $*
+pushdef(`rs', __CHIP8_REGS_$1)dnl
+ifelse(rs, `15', `errprint(`PUSHREG: too many registers: $*
 ')m4exit(1)')dnl
-pushdef($2, `_R'_$1)dnl
-`  # PUSHREG 'defn(`_R'`'_$1)` $*'dnl
-define(`_$1', incr(_$1))dnl
+pushdef(`$2', `__CHIP8_R'rs)dnl
+pushdef(`__CHIP8_REGS_$1', `$2')dnl
+`  # PUSHREG '$2` $*'dnl
+pushdef(`__CHIP8_REGS_$1', incr(rs))dnl
+popdef(`rs')dnl
 ')dnl
+define(`REGSLVL', `__CHIP8_REGS_$1')dnl
 dnl
 dnl POPREGS(regset, oregset)
 dnl
 define(`POPREGS', `dnl
   # POPREGS $*
-FOR(`IX', $2, `decr(_$1)', `dnl
-	popdef(defn(`_$1'`'IX))dnl
-	popdef(`_$1'`'IX)'dnl
-)dnl'
-popdef(`$1')dnl
+FOR(`IX', $2, `decr(__CHIP8_REGS_$1)', `dnl
+	popdef(`__CHIP8_REGS_$1')dnl
+	popdef(defn(`__CHIP8_REGS_$1'))dnl
+	popdef(`__CHIP8_REGS_$1')'dnl
 )dnl
+dnl ifelse(`__CHIP8_REGS_$1', $2, popdef(`__CHIP8_REGS_$1'))dnl
+')dnl
 dnl
 dnl vim: set filetype=m4: #modeline
