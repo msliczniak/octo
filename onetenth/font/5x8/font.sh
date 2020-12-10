fc63ce48609ce7dbdc557c9fb83f8657bb78467b
mzs <mzs@laptop>
2020-12-10 16:22:19 -0600

    bitplanes

---
+++ onetenth/font/5x8/font.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# create m4 template from GIF
+
+convert "$1" -crop 16x7+0+1 -compress none pbm:- | \
+  awk 'BEGIN { print "ibase=0000000002" } NR > 2 { print }' | \
+  fmt -16 | tr -d ' ' | bc | awk 'BEGIN {
+	print "dnl", "'"$1"'"
+}
+
+{
+	printf("pushdef(`L%d'"'"', %s)", i, $1)
+	getline
+	printf("pushdef(`R%d'"'"', %s)dnl\n", i, $1)
+	i++
+}
+
+END {
+	print "NI"
+	print ""
+}'
