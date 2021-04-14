247da5e1e0819f0848d6647b8be48d60eddf63f1
Agnes Sliczniak <aga@admins-Air.local>
2021-04-14 17:25:37 -0500

    proper xor

--- picross/slice.sh
+++ picross/slice.sh
@@ -7,9 +7,12 @@
 # for i in shroom?-*??.pbm; do echo ': '`basename $i .pbm`; \
 # tail -15 "$i" | tr -d ' ' | sed 's/^/0b/'; echo; done
 
-convert "$1".pbm -sample 45x45 pbm:- | \
+n=`basename "$1" .pbm` || exit
+shift 1
+
+convert "$n".pbm -sample 45x45 pbm:- | \
 convert -size 48x45 xc:white pbm:- -geometry +2+0 -composite \
-  -draw 'line 47,0 47,44' -compress None -strip pbm:- | \
+  $* -strokewidth 1 -draw 'line 47,0 47,44' -compress None -strip pbm:- | \
 convert pbm:- -crop 8x15 -compress None -strip pbm:- | \
 awk '/P1/ { close(f); f = sprintf("%s-%02d.pbm", n, i++) } { print >f }
-	END { if (i != 18) exit(1) }' n="$1"
+	END { if (i != 18) exit(1) }' n="$n"
