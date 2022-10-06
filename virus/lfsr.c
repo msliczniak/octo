27745029a26e060e87b833c313f34f363ec74d98
Mike Sliczniak <mzs>
2022-10-06 15:55:19 -0500

    all seeds

--- virus/lfsr.c
+++ virus/lfsr.c
@@ -9,20 +9,30 @@ main(int argc, char *argv[])
 	unsigned int ui;
 	uint8_t seed[2];
 
-	if (argc != 3) return (2);
-	if (sscanf(argv[1], "%x", &ui) != 1) return (3);
-	seed[0] = lfsr[0] = ui;
-	if (sscanf(argv[2], "%x", &ui) != 1) return (4);
-	seed[1] = lfsr[1] = ui;
+	
+	(void)argc; (void)argv;
+	seed[0] = 0x80; seed[1] = 1;
 	for (;;) {
-		lfsr[0] <<= 1;
-		if (lfsr[1] & 0x80) lfsr[0] |= 1;
-		lfsr[1] <<= 1;
-		if ((uint8_t)((lfsr[0] & 0x80) + (lfsr[1] & 0x80)))
-			lfsr[1] |= 1;
-		(void)printf("%02x%02x\n", lfsr[0], lfsr[1]);
-		if (lfsr[0] != seed[0]) continue;
-		if (lfsr[1] != seed[1]) continue;
-		return (0);
+		lfsr[0] = seed[0]; lfsr[1] = seed[1];
+		(void)printf("%02x%02x", seed[0], seed[1]);
+		for (;;) {
+			lfsr[0] <<= 1;
+			if (lfsr[1] & 0x80) lfsr[0] |= 1;
+			lfsr[1] <<= 1;
+			if ((uint8_t)((lfsr[0] & 0x80) + (lfsr[1] & 0x80)))
+				lfsr[1] |= 1;
+			(void)printf(" %02x%02x", lfsr[0], lfsr[1]);
+
+			if (lfsr[0] != seed[0]) continue;
+			if (lfsr[1] != seed[1]) continue;
+			break;
+		}
+
+		(void)printf("\n");
+		if ((seed[0] == 0x7f) && (seed[1] == 0xff)) return (0);
+
+		seed[1] += 1;
+		if (seed[1] == 0) seed[0] += 1;
+		if ((uint8_t)(seed[1] << 1)) seed[0] += 0x80;
 	}
 }
