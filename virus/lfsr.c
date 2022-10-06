e610c73d2ab9d6f9afcd557ae4f5c6991bf26fff
Agnes Sliczniak <aga@admins-MBP.local>
2022-10-06 00:17:15 -0500

    i wraps after 4k sadly in xo-chip

--- virus/lfsr.c
+++ virus/lfsr.c
@@ -1,25 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define SEED0 2
-#define SEED1 2
-
 static uint8_t lfsr[2];
 
 int
 main(int argc, char *argv[])
 {
-	(void)argc; (void)argv;
+	unsigned int ui;
+	uint8_t seed[2];
 
-	lfsr[0] = SEED0; lfsr[1] = SEED1;
+	if (argc != 3) return (2);
+	if (sscanf(argv[1], "%x", &ui) != 1) return (3);
+	seed[0] = lfsr[0] = ui;
+	if (sscanf(argv[2], "%x", &ui) != 1) return (4);
+	seed[1] = lfsr[1] = ui;
 	for (;;) {
 		lfsr[0] <<= 1;
 		if (lfsr[1] & 0x80) lfsr[0] |= 1;
 		lfsr[1] <<= 1;
-		if ((lfsr[0] & 0x80) ^ (lfsr[1] & 0x80)) lfsr[1] |= 1;
+		if ((uint8_t)((lfsr[0] & 0x80) + (lfsr[1] & 0x80)))
+			lfsr[1] |= 1;
 		(void)printf("%02x%02x\n", lfsr[0], lfsr[1]);
-		if (lfsr[0] != SEED0) continue;
-		if (lfsr[1] != SEED1) continue;
+		if (lfsr[0] != seed[0]) continue;
+		if (lfsr[1] != seed[1]) continue;
 		return (0);
 	}
 }
